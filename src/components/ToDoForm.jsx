9692b18b845fdc566bd6b2b0148d5bf57ab1a474
mitul72 <mituldhawan2@gmail.com>
2024-04-12 17:09:46 -0600

    add lab6

--- src/components/ToDoForm.jsx
+++ src/components/ToDoForm.jsx
@@ -1,22 +1,37 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import {View, TextInput, Button, StyleSheet} from 'react-native';
+import Tasks from '../data/tasks.json';
+
 export default function ToDoForm({tasks, setTasks, addTask}) {
+  const [randomTasks, setRandomTasks] = useState([]);
+  useEffect(() => {
+    setRandomTasks(Tasks.tasks);
+  }, []);
+  selectRandomTask = () => {
+    const randomIndex = Math.floor(Math.random() * randomTasks.length);
+    setNewTask(randomTasks[randomIndex]);
+  };
+
   const [newTask, setNewTask] = useState('');
   return (
-    <View style={styles.form}>
-      <TextInput
-        value={newTask}
-        onChangeText={text => setNewTask(text)}
-        style={styles.input}
-        placeholder="Add a new task..."
-      />
-      <Button
-        onPress={() => {
-          addTask(newTask);
-          setNewTask('');
-        }}
-        title="Add"
-      />
+    <View style={{display: 'flex', gap: 10}}>
+      <View style={styles.form}>
+        <TextInput
+          value={newTask}
+          onChangeText={text => setNewTask(text)}
+          style={styles.input}
+          placeholder="Add a new task..."
+        />
+        <Button
+          onPress={() => {
+            addTask(newTask);
+            setNewTask('');
+          }}
+          title="Add"
+        />
+      </View>
+
+      <Button onPress={selectRandomTask} title="Generate Random Task" />
     </View>
   );
 }
